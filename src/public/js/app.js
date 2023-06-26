833d6612ad2ab6c4c3f135ff1695d62a050817b3
3Harang <jungjaeseung9475@gmail.com>
2023-06-26 14:38:25 +0900

    Realtime Chat Using WebSocket
    realtime chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,24 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener("open", () =>
+  console.log("WebSocket connected to Server")
+);
+
+socket.addEventListener("message", (message) =>
+  console.log("New Message from WS :", message.data)
+);
+
+socket.addEventListener("close", () =>
+  console.log("WebSocket disconnected to Server")
+);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", onSubmit);
